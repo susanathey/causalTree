2fd00351f6551fee7844cd5bb3ff3314041a0f70
susanathey <athey@stanford.edu>
2016-05-07 15:25:25 -0700

    Commit Random Forest Code

--- R/causalForest/causalForest.R
+++ R/causalForest/causalForest.R
@@ -1,13 +1,12 @@
-library(causalTree)
+library(causalTree) 
+library(rpart)
 
 init.causalForest <- function(formula, data, treatment, weights=F, cost=F, num.trees) { 
   num.obs <- nrow(data)
+  # this is a hack to fix global environment problem for formula; need to find more elegant solution
   formform <- as.formula(paste(as.character(formula[2]),as.character(formula[1]),as.character(formula[3])))
-  print(formform)
   trees <- vector("list", num.trees)
-  print(trees)
   inbag <- matrix(0, num.obs, num.trees) 
-  print(inbag)
   causalForestobj <- list(trees = trees, formula=formform, data=data, treatment=treatment, weights=weights, cost=cost, ntree = num.trees, inbag = inbag) 
   class(causalForestobj) <- "causalForest" 
   return(causalForestobj)
@@ -32,22 +31,24 @@ causalForest <- function(formula, data, treatment,
                          na.action = na.causalTree, 
                          split.Rule="CT", split.Honest=T, split.Bucket=F, bucketNum = 5,
                          bucketMax = 100, cv.option="CT", cv.Honest=T, minsize = 2L, 
-                         propensity, control, split.alpha = 0.5, cv.alpha = 0.5,  
+                         propensity, control, split.alpha = 0.5, cv.alpha = 0.5,
                          
                          sample.size.total = floor(nrow(data) / 10), sample.size.train.frac = .5,
                          mtry = ceiling(ncol(data)/3), nodesize = 1, num.trees=nrow(data),
                          cost=F, weights=F) {
   
-  # do not implement subset option of causalTree, inherited from rpart
+  # do not implement subset option of causalTree, that is inherited from rpart but have not implemented it here yet
 
   num.obs <-nrow(data)
-  formula2 <- as.formula(paste(as.character(formula[2]),as.character(formula[1]),as.character(formula[3])))
-  causalForest.hon <- init.causalForest(formula=formula2, data=data, treatment=treatment, weights=weights, cost=cost, num.trees=num.trees)
+  # this is a hack to fix global environment problem for formula; need to find more elegant solution
+  formform <- as.formula(paste(as.character(formula[2]),as.character(formula[1]),as.character(formula[3])))
+  
+  causalForest.hon <- init.causalForest(formula=formform, data=data, treatment=treatment, weights=weights, cost=cost, num.trees=num.trees)
   sample.size <- min(sample.size.total, num.obs)
   train.size <- round(sample.size.train.frac*sample.size)
   est.size <- sample.size - train.size
   
-  formform <- as.formula(paste(as.character(formula[2]),as.character(formula[1]),as.character(formula[3])))
+
   treatmentdf <- data.frame(treatment)
   
   print("Building trees ...")
@@ -73,7 +74,7 @@ causalForest <- function(formula, data, treatment,
                                      split.alpha = 0.5, cv.alpha = 0.5, xval=0, 
                                      HonestSampleSize=est.size, cp=0)
 
-    print(tree.honest)
+    #print(tree.honest)
     causalForest.hon$trees[[tree.index]] <- tree.honest
     causalForest.hon$inbag[full.idx, tree.index] <- 1
   }
@@ -86,13 +87,13 @@ propensityForest <- function(formula, data, treatment,
                          na.action = na.causalTree, 
                          split.Rule="CT", split.Honest=T, split.Bucket=F, bucketNum = 5,
                          bucketMax = 100, cv.option="CT", cv.Honest=T, minsize = 2L, 
-                         propensity, control, split.alpha = 0.5, cv.alpha = 0.5,  
+                         propensity=mean(treatment), control, split.alpha = 0.5, cv.alpha = 0.5,  
                          
                          sample.size.total = floor(nrow(data) / 10), sample.size.train.frac = 1,
-                         mtry = ceiling(ncol(data)/3), nodesize = 1, num.trees=nrow(data),
-                         cost=F, weights=F) {
+                         mtry = ceiling(ncol(data)/3), nodesize = 1, num.trees=nrow(data)) {
   
   # do not implement subset option of causalTree, inherited from rpart
+  # do not implement weights and costs yet
   
   if(sample.size.train.frac != 1) {
     print("warning: for propensity Forest, sample.size.train.frac should be 1; resetting to 1")
@@ -100,13 +101,16 @@ propensityForest <- function(formula, data, treatment,
   }
   
   num.obs <-nrow(data)
-  formula2 <- as.formula(paste(as.character(formula[2]),as.character(formula[1]),as.character(formula[3])))
-  causalForest.hon <- init.causalForest(formula=formula2, data=data, treatment=treatment, weights=weights, cost=cost, num.trees=num.trees)
+  
+  # this is a hack to fix global environment problem for formula; need to find more elegant solution; but do have to change formula anyway
+  formform <- as.formula(paste(as.character(formula[2]),as.character(formula[1]),as.character(formula[3])))
+  
+  causalForest.hon <- init.causalForest(formula=formform, data=data, treatment=treatment, num.trees=num.trees, weights=F, cost=F)
   sample.size <- min(sample.size.total, num.obs)
   train.size <- round(sample.size.train.frac*sample.size)
   
-  formform <- as.formula(paste(as.character(formula[2]),as.character(formula[1]),as.character(formula[3])))
   treatmentdf <- data.frame(treatment)
+  outcomename = as.character(formula[2])
   
   print("Building trees ...")
   
@@ -117,23 +121,34 @@ propensityForest <- function(formula, data, treatment,
     full.idx <- sample.int(num.obs, sample.size, replace = FALSE)
     train.idx <- full.idx[1:train.size]
     
+    # rename variables as a way to trick rpart into building the tree with all the object attributes considering the outcome variable as named
+    # by the input formula, even though the tree itself is trained on w.  Note that we aren't saving out this propensity tree anyway, but if
+    # we decided later to try to save out the propensity trees and do something directly with the propensity scores, we would need to do something
+    # more tedious like estimate the propensity tree with the original names, and then edit the attributes to replace the treatment variable name
+    # with the outcome variable name for the estimate part
     dataTree <- data.frame(data[train.idx,])
-    dataEstim <- data.frame(data[reestimation.idx,])
+    dataTree$treattreat <- treatmentdf[train.idx,]
+    names(dataTree)[names(dataTree)==outcomename] <- "temptemp"
+
+    names(dataTree)[names(dataTree)=="treattreat"] <- outcomename
     
-    tree.propensity <- rpart()
     
-    tree.honest <- honest.causalTree(formula=formform, data = dataTree, 
-                                     treatment = treatmentdf[train.idx,], 
-                                     est_data=dataEstim, est_treatment=treatmentdf[reestimation.idx,],
-                                     split.Rule="CT", split.Honest=T, split.Bucket=split.Bucket, 
-                                     bucketNum = bucketNum, 
-                                     bucketMax = bucketMax, cv.option="CT", cv.Honest=T, 
-                                     minsize = nodesize, 
-                                     split.alpha = 0.5, cv.alpha = 0.5, xval=0, 
-                                     HonestSampleSize=est.size, cp=0)
+    #one options: estimate the propensity tree with anova so that it will be type "anova" when we re-estimate
+    #here: replace elements of the rpart object to make it look like anova tree, so that we'll be able to properly predict with it later, etc.
+    tree.propensity <- rpart(formula=formform, data=dataTree, method="class", 
+                             control=rpart.control(cp=0, minbucket=nodesize))
     
-    print(tree.honest)
-    causalForest.hon$trees[[tree.index]] <- tree.honest
+    # make it look like a method="anova" tree 
+    tree.propensity$method <- "anova"
+    tree.propensity$frame$yval2 <- NULL
+    tree.propensity$functions$print <- NULL
+    
+    # switch the names back in the data frame so that when we estimate treatment effects, will have the right outcome variables
+    names(dataTree)[names(dataTree)==outcomename] <- "treattreat"
+    names(dataTree)[names(dataTree)=="temptemp"] <- outcomename
+    tree.treatment <- estimate.causalTree(object=tree.propensity,data=dataTree, treatment=dataTree$treattreat)
+    
+    causalForest.hon$trees[[tree.index]] <- tree.treatment
     causalForest.hon$inbag[full.idx, tree.index] <- 1
   }
   
