fd8a5ed1487f3425440b16795c0e3750920560f2
susanathey <athey@stanford.edu>
2016-05-07 11:22:46 -0700

    causalForest code

--- R/causalForest/causalForest.R
+++ R/causalForest/causalForest.R
@@ -1,28 +1,54 @@
 library(causalTree)
 
-init.causalForest <- function(x, y, w, num.trees) { 
-  num.obs <- nrow(x)
-  print("init")
-  print(num.obs)
-  print(num.trees)
-  trees <- vector("list", num.trees) 
+init.causalForest <- function(formula, data, treatment, weights=F, cost=F, num.trees) { 
+  num.obs <- nrow(data)
+  formform <- as.formula(paste(as.character(formula[2]),as.character(formula[1]),as.character(formula[3])))
+  print(formform)
+  trees <- vector("list", num.trees)
+  print(trees)
   inbag <- matrix(0, num.obs, num.trees) 
-  causalForest <- list(trees = trees, x = x, y = y, w = w, ntree = num.trees, inbag = inbag) 
-  class(causalForest) <- "causalForest" 
+  print(inbag)
+  causalForestobj <- list(trees = trees, formula=formform, data=data, treatment=treatment, weights=weights, cost=cost, ntree = num.trees, inbag = inbag) 
+  class(causalForestobj) <- "causalForest" 
+  return(causalForestobj)
 } 
 
-causalForest <- function(X, Y, W, num.trees, sample.size = floor(length(Y) / 10), 
-                         mtry = ceiling(ncol(X)/3), nodesize = 1, split.Bucket=F,
-                         bucketMax=100, bucketNum=5) {
+predict.causalForest <- function(forest, newdata, predict.all = FALSE, type="vector") {
+  if (!inherits(forest, "causalForest")) stop("Not a legitimate \"causalForest\" object")  
+
+  individual <- sapply(forest$trees, function(tree.fit) {
+    predict(tree.fit, newdata=newdata, type="vector")
+  })
   
-  if (any(is.na(X)) || any(is.na(Y)) || any(is.na(W))) {
-    stop("There are missing values in the input.")
+  aggregate <- rowMeans(individual)
+  if (predict.all) {
+    list(aggregate = aggregate, individual = individual)
+  } else {
+    aggregate
   }
+}
+
+causalForest <- function(formula, data, treatment,  
+                         na.action = na.causalTree, 
+                         split.Rule="CT", split.Honest=T, split.Bucket=F, bucketNum = 5,
+                         bucketMax = 100, cv.option="CT", cv.Honest=T, minsize = 2L, 
+                         propensity, control, split.alpha = 0.5, cv.alpha = 0.5,  
+                         
+                         sample.size.total = floor(nrow(data) / 10), sample.size.train.frac = .5,
+                         mtry = ceiling(ncol(data)/3), nodesize = 1, num.trees=nrow(data),
+                         cost=F, weights=F) {
   
-  num.obs <-nrow(X)
-  print(c("num.obs", num.obs))
-  causalForest.honest <- init.causalForest(x=X, y=Y, w=W, num.trees=num.trees)
-  sample.size <- min(sample.size, floor(num.obs / 2))
+  # do not implement subset option of causalTree, inherited from rpart
+
+  num.obs <-nrow(data)
+  formula2 <- as.formula(paste(as.character(formula[2]),as.character(formula[1]),as.character(formula[3])))
+  causalForest.hon <- init.causalForest(formula=formula2, data=data, treatment=treatment, weights=weights, cost=cost, num.trees=num.trees)
+  sample.size <- min(sample.size.total, num.obs)
+  train.size <- round(sample.size.train.frac*sample.size)
+  est.size <- sample.size - train.size
+  
+  formform <- as.formula(paste(as.character(formula[2]),as.character(formula[1]),as.character(formula[3])))
+  treatmentdf <- data.frame(treatment)
   
   print("Building trees ...")
   
@@ -30,25 +56,86 @@ causalForest <- function(X, Y, W, num.trees, sample.size = floor(length(Y) / 10)
     
     print(paste("Tree", as.character(tree.index)))
     
-    full.idx <- sample.int(num.obs, 2 * sample.size, replace = FALSE)
-    train.idx <- full.idx[1:sample.size]
-    reestimation.idx <- full.idx[sample.size + (1:sample.size)]
+    full.idx <- sample.int(num.obs, sample.size, replace = FALSE)
+    train.idx <- full.idx[1:train.size]
+    reestimation.idx <- full.idx[(train.size+1):sample.size]
     
-    tree.DF = data.frame(Y = Y, X = X)
-    
-    tree.honest <- honest.causalTree(Y~., data = tree.DF[train.idx,], 
-                                     treatment = W[train.idx], 
-                                     est_data=tree.DF[reestimation.idx], est_treatment=W[reestimation.idx],
+    dataTree <- data.frame(data[train.idx,])
+    dataEstim <- data.frame(data[reestimation.idx,])
+
+    tree.honest <- honest.causalTree(formula=formform, data = dataTree, 
+                                     treatment = treatmentdf[train.idx,], 
+                                     est_data=dataEstim, est_treatment=treatmentdf[reestimation.idx,],
                                      split.Rule="CT", split.Honest=T, split.Bucket=split.Bucket, 
                                      bucketNum = bucketNum, 
                                      bucketMax = bucketMax, cv.option="CT", cv.Honest=T, 
                                      minsize = nodesize, 
                                      split.alpha = 0.5, cv.alpha = 0.5, xval=0, 
-                                     HonestSampleSize=nrow(reestimation.idx), cp=0)
-    
-    causalForest.honest$trees[[tree.index]] <- tree.honest
-    causalForest.honest$inbag[full.idx, tree.index] <- 1
+                                     HonestSampleSize=est.size, cp=0)
+
+    print(tree.honest)
+    causalForest.hon$trees[[tree.index]] <- tree.honest
+    causalForest.hon$inbag[full.idx, tree.index] <- 1
   }
   
-  return(causalForest.honest)
+  return(causalForest.hon)
+}
+
+
+propensityForest <- function(formula, data, treatment,  
+                         na.action = na.causalTree, 
+                         split.Rule="CT", split.Honest=T, split.Bucket=F, bucketNum = 5,
+                         bucketMax = 100, cv.option="CT", cv.Honest=T, minsize = 2L, 
+                         propensity, control, split.alpha = 0.5, cv.alpha = 0.5,  
+                         
+                         sample.size.total = floor(nrow(data) / 10), sample.size.train.frac = 1,
+                         mtry = ceiling(ncol(data)/3), nodesize = 1, num.trees=nrow(data),
+                         cost=F, weights=F) {
+  
+  # do not implement subset option of causalTree, inherited from rpart
+  
+  if(sample.size.train.frac != 1) {
+    print("warning: for propensity Forest, sample.size.train.frac should be 1; resetting to 1")
+    sample.size.train.frac <- 1
+  }
+  
+  num.obs <-nrow(data)
+  formula2 <- as.formula(paste(as.character(formula[2]),as.character(formula[1]),as.character(formula[3])))
+  causalForest.hon <- init.causalForest(formula=formula2, data=data, treatment=treatment, weights=weights, cost=cost, num.trees=num.trees)
+  sample.size <- min(sample.size.total, num.obs)
+  train.size <- round(sample.size.train.frac*sample.size)
+  
+  formform <- as.formula(paste(as.character(formula[2]),as.character(formula[1]),as.character(formula[3])))
+  treatmentdf <- data.frame(treatment)
+  
+  print("Building trees ...")
+  
+  for (tree.index in 1:num.trees) {
+    
+    print(paste("Tree", as.character(tree.index)))
+    
+    full.idx <- sample.int(num.obs, sample.size, replace = FALSE)
+    train.idx <- full.idx[1:train.size]
+    
+    dataTree <- data.frame(data[train.idx,])
+    dataEstim <- data.frame(data[reestimation.idx,])
+    
+    tree.propensity <- rpart()
+    
+    tree.honest <- honest.causalTree(formula=formform, data = dataTree, 
+                                     treatment = treatmentdf[train.idx,], 
+                                     est_data=dataEstim, est_treatment=treatmentdf[reestimation.idx,],
+                                     split.Rule="CT", split.Honest=T, split.Bucket=split.Bucket, 
+                                     bucketNum = bucketNum, 
+                                     bucketMax = bucketMax, cv.option="CT", cv.Honest=T, 
+                                     minsize = nodesize, 
+                                     split.alpha = 0.5, cv.alpha = 0.5, xval=0, 
+                                     HonestSampleSize=est.size, cp=0)
+    
+    print(tree.honest)
+    causalForest.hon$trees[[tree.index]] <- tree.honest
+    causalForest.hon$inbag[full.idx, tree.index] <- 1
+  }
+  
+  return(causalForest.hon)
 }
