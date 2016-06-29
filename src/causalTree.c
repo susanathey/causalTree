37a25e0d9a9ebeb87b28a9241dc8c5c18a582907
vikas84bf <vikas.ramachandra@gmail.com>
2016-06-29 10:21:18 -0700

    .

--- src/causalTree.c
+++ src/causalTree.c
@@ -127,7 +127,7 @@ causalTree(SEXP ncat2, SEXP split_Rule2, SEXP bucketnum2, SEXP bucketMax2, SEXP 
     char getchar1;
     
     R_FlushConsole();
-    R_Process();
+    //R_Process();
     Rprintf("test print\n");
     printf("test print2\n");
     getchar1=getchar();
